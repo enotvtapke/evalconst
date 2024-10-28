5cb83aa8c7741899b22ff8bd99c61977fbc5dafe
enotvtapke <a.stupnikov05@gmail.com>
2024-10-28 11:06:17 +0100

    Add modules for compiler plugin and gradle plugin

---
+++ evalconst-gradle-plugin/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "internship"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
